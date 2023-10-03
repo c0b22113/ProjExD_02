fca1b239785bb133398324f4bff7af7da5960705
c0b22113 <c0b2211320@edu.teu.ac.jp>
2023-10-03 15:23:42 +0900

    練習問題 4後半 5

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -6,7 +6,7 @@ import pygame as pg
 WIDTH, HEIGHT = 1600, 900
 
 
-delta = { # 練習3：キー入力に対する移動量
+delta = {  # 練習３：移動量辞書
     pg.K_UP: (0, -5),
     pg.K_DOWN: (0, +5),
     pg.K_LEFT: (-5, 0),
@@ -14,71 +14,70 @@ delta = { # 練習3：キー入力に対する移動量
 }
 
 
-#  こうかとんと爆弾が画面の外に出ないようにせよ．
-# • 画面内or画面外の判定をする関数を実装する
-# • 引数：こうかとんRect or 爆弾Rect
-# • 戻り値：横方向・縦方向の真理値タプル（True：画面内／False：画面外）
-# • Rectオブジェクトのleft, right, top, bottomの値から画面内・外を判断する
-# • 更新後の座標が画面外になった場合の挙動
-# • こうかとん：更新前の位置に戻す／爆弾：速度の符号を反転する
 def check_bound(obj_rct: pg.Rect):
     """
-    引数:こうかとんRect or 爆弾Rect
-    戻り値:横方向・縦方向の真理値タプル（True:画面内/False:画面外）
+    引数：こうかとんRectかばくだんRect
+    戻り値：タプル（横方向判定結果，縦方向判定結果）
+    画面内ならTrue，画面外ならFalse
     """
-    status = [True, True]
-    if obj_rct.left < 0 or WIDTH < obj_rct.right:
-        status[0] = False
-        return status
-    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:
-        status[1] = False
-        return status
+    yoko, tate = True, True
+    if obj_rct.left < 0 or WIDTH < obj_rct.right: # 横方向判定
+        yoko = False
+    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom: # 縦方向判定
+        tate = False
+    return yoko, tate
 
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
-
     """こうかとん"""
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-    kk_img_rct = kk_img.get_rect()
-    kk_img_rct.center = (900, 400) # こうかとんの初期位置
-
-    """爆弾"""
-    bomb = pg.Surface((20, 20))
-    bomb.set_colorkey((0, 0, 0))
-    pg.draw.circle(bomb, (255, 0, 0), (10, 10), 10)
-    bomb_rct = bomb.get_rect()
-    x, y = random.randint(0, WIDTH), random.randint(0, HEIGHT) # 画面サイズにランダムに配置
-    bomb_rct.center = (x, y) # 画面中央に配置
-    vx, vy = 5, 5 # 速度
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = (900, 400)  # 練習３：こうかとんの初期座標を設定する
+    """ばくだん"""
+    bd_img = pg.Surface((20, 20))  # 練習１：爆弾Surfaceを作成する
+    bd_img.set_colorkey((0, 0, 0))  # 練習１：黒い部分を透明にする
+    pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
+    bd_rct = bd_img.get_rect()  # 練習１：SurfaceからRectを抽出する
+    x, y = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    bd_rct.center = (x, y)  # 練習１：Rectにランダムな座標を設定する
+    vx, vy = +5, +5  # 練習２：爆弾の速度
 
     clock = pg.time.Clock()
-
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
 
+        if kk_rct.colliderect(bd_rct): # 衝突判定
+            print("GAME OVER")
+            return
+
         screen.blit(bg_img, [0, 0])
 
         """こうかとん"""
         key_lst = pg.key.get_pressed()
-        total_mv = [0, 0]
+        sum_mv = [0, 0]
         for key, mv in delta.items():
             if key_lst[key]:
-                total_mv[0] += mv[0]
-                total_mv[1] += mv[1]
-
-        screen.blit(kk_img, kk_img_rct)
-        kk_img_rct.move_ip(total_mv[0], total_mv[1]) # こうかとんの移動
-
-        """爆弾"""
-        screen.blit(bomb, bomb_rct)
-        bomb_rct.move_ip(vx, vy) # bombの移動
+                sum_mv[0] += mv[0]  # 練習３：横方向の合計移動量
+                sum_mv[1] += mv[1]  # 練習３：縦方向の合計移動量
+        kk_rct.move_ip(sum_mv[0], sum_mv[1])  # 練習３：移動させる
+        if check_bound(kk_rct) != (True, True):  # 練習４：はみだし判定
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+        screen.blit(kk_img, kk_rct)  # 練習３：移動後の座標に表示させる
+        """"ばくだん"""
+        bd_rct.move_ip(vx, vy)  # 練習２：爆弾を移動させる
+        yoko, tate = check_bound(bd_rct)
+        if not yoko:  # 練習４：横方向にはみ出たら
+            vx *= -1
+        if not tate:  # 練習４：縦方向にはみ出たら
+            vy *= -1
+        screen.blit(bd_img, bd_rct)  # 練習１：Rectを使って試しにblit
 
         pg.display.update()
         tmr += 1
