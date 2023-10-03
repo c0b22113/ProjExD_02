dc8f6d1170b064ac46926db9dab1f449a351abae
c0b22113 <c0b2211320@edu.teu.ac.jp>
2023-10-03 14:17:20 +0900

    練習問題 1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,7 +1,9 @@
+import random
 import sys
 import pygame as pg
 
 WIDTH, HEIGHT = 1600, 900
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -9,7 +11,16 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+
+    bomb = pg.Surface((20, 20))
+    bomb.set_colorkey((0, 0, 0))
+    pg.draw.circle(bomb, (255, 0, 0), (10, 10), 10)
+    bomb_rct = bomb.get_rect()
+    x, y = random.randint(0, WIDTH), random.randint(0, HEIGHT) # 画面サイズにランダムに配置
+    bomb_rct.center = (x, y) # 画面中央に配置
+
     clock = pg.time.Clock()
+
     tmr = 0
     while True:
         for event in pg.event.get():
@@ -18,9 +29,11 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(bomb, bomb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(10)
+
 
 if __name__ == "__main__":
     pg.init()
