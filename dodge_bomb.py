de114e3d253c1dc2817cb53f952aaa7adf8f3e0e
c0b22113 <c0b2211320@edu.teu.ac.jp>
2023-10-03 16:04:13 +0900

    追加機能 1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -2,11 +2,9 @@ import random
 import sys
 import pygame as pg
 
-
 WIDTH, HEIGHT = 1600, 900
 
-
-delta = {  # 練習３：移動量辞書
+delta = {
     pg.K_UP: (0, -5),
     pg.K_DOWN: (0, +5),
     pg.K_LEFT: (-5, 0),
@@ -16,68 +14,137 @@ delta = {  # 練習３：移動量辞書
 
 def check_bound(obj_rct: pg.Rect):
     """
-    引数：こうかとんRectかばくだんRect
-    戻り値：タプル（横方向判定結果，縦方向判定結果）
-    画面内ならTrue，画面外ならFalse
+    引数: こうかとんRect or 爆弾Rect
+    戻り値: タプル(横方向の衝突判定, 縦方向の衝突判定)
+    画面ないならTrue, 画面外ならFalse
     """
     yoko, tate = True, True
-    if obj_rct.left < 0 or WIDTH < obj_rct.right: # 横方向判定
+    if obj_rct.left < 0 or WIDTH < obj_rct.right:
         yoko = False
-    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom: # 縦方向判定
+    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:
         tate = False
     return yoko, tate
+
+
+def load_and_convert_images():
+    """
+    戻り値: 背景画像, こうかとん画像
+    """
+    bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
+    kk_img = pg.image.load("ex02/fig/3.png")
+    kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    return bg_img, kk_img
+
+
+def create_bomb():
+    """
+    戻り値: 爆弾画像, 爆弾Rect
+    """
+    bd_img = pg.Surface((20, 20))
+    bd_img.set_colorkey((0, 0, 0))
+    pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
+    bd_rct = bd_img.get_rect()
+    x, y = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    bd_rct.center = (x, y)
+    return bd_img, bd_rct
+
+
+def get_rotation_angle_and_flip(mv):
+    """
+    引数: 移動量 (mv)
+    戻り値: 回転角度, 反転フラグ
+    """
+    angles = {
+        (0, -5): (90, True),
+        (-5, -5): (-45, False),
+        (-5, 0): (0, False),
+        (-5, +5): (45, False),
+        (0, +5): (-90, True),
+        (+5, +5): (-45, True),
+        (+5, 0): (0, True),
+        (+5, -5): (45, True)
+    }
+    return angles.get(mv, (0, False))
+
+
+def move_character(kk_rct, delta):
+    """
+    引数: こうかとんRect, キー入力に対応した移動量
+    戻り値: こうかとんの移動量, 回転角度, 反転フラグ
+    """
+    key_lst = pg.key.get_pressed()
+    sum_mv = [0, 0]
+    for key, mv in delta.items():
+        if key_lst[key]:
+            sum_mv[0] += mv[0]
+            sum_mv[1] += mv[1]
+
+    rotation_angle, flip = get_rotation_angle_and_flip(tuple(sum_mv))
+
+    kk_rct.move_ip(sum_mv[0], sum_mv[1])
+    if check_bound(kk_rct) != (True, True):
+        kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+
+    return sum_mv, rotation_angle, flip
+
+
+def move_bomb(bd_rct, vx, vy):
+    """
+    引数: 爆弾Rect, 爆弾の移動量
+    戻り値: 爆弾の移動量
+    """
+    bd_rct.move_ip(vx, vy)
+    yoko, tate = check_bound(bd_rct)
+    if not yoko:
+        vx *= -1
+    if not tate:
+        vy *= -1
+    return vx, vy
+
+
+def draw_images(screen, bg_img, kk_img, kk_rct, bd_img, bd_rct, rotation_angle, flip):
+    """
+    引数：スクリーン, 背景画像, こうかとん画像, こうかとんRect, 爆弾画像, 爆弾Rect, 回転角度, 反転フラグ
+    """
+    screen.blit(bg_img, [0, 0])
+
+    kk_img_to_draw = kk_img
+    if flip:
+        kk_img_to_draw = pg.transform.flip(kk_img, True, False)
+    kk_img_to_draw = pg.transform.rotozoom(kk_img_to_draw, rotation_angle, 1.0)
+
+    screen.blit(kk_img_to_draw, kk_rct)
+    screen.blit(bd_img, bd_rct)
+
 
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
-    bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
-    """こうかとん"""
-    kk_img = pg.image.load("ex02/fig/3.png")
-    kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+
+    bg_img, kk_img = load_and_convert_images()
+
     kk_rct = kk_img.get_rect()
-    kk_rct.center = (900, 400)  # 練習３：こうかとんの初期座標を設定する
-    """ばくだん"""
-    bd_img = pg.Surface((20, 20))  # 練習１：爆弾Surfaceを作成する
-    bd_img.set_colorkey((0, 0, 0))  # 練習１：黒い部分を透明にする
-    pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
-    bd_rct = bd_img.get_rect()  # 練習１：SurfaceからRectを抽出する
-    x, y = random.randint(0, WIDTH), random.randint(0, HEIGHT)
-    bd_rct.center = (x, y)  # 練習１：Rectにランダムな座標を設定する
-    vx, vy = +5, +5  # 練習２：爆弾の速度
+    kk_rct.center = (900, 400)
+
+    bd_img, bd_rct = create_bomb()
+    vx, vy = +5, +5
 
     clock = pg.time.Clock()
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
 
-        if kk_rct.colliderect(bd_rct): # 衝突判定
+        if kk_rct.colliderect(bd_rct):
             print("GAME OVER")
             return
 
-        screen.blit(bg_img, [0, 0])
-
-        """こうかとん"""
-        key_lst = pg.key.get_pressed()
-        sum_mv = [0, 0]
-        for key, mv in delta.items():
-            if key_lst[key]:
-                sum_mv[0] += mv[0]  # 練習３：横方向の合計移動量
-                sum_mv[1] += mv[1]  # 練習３：縦方向の合計移動量
-        kk_rct.move_ip(sum_mv[0], sum_mv[1])  # 練習３：移動させる
-        if check_bound(kk_rct) != (True, True):  # 練習４：はみだし判定
-            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
-        screen.blit(kk_img, kk_rct)  # 練習３：移動後の座標に表示させる
-        """"ばくだん"""
-        bd_rct.move_ip(vx, vy)  # 練習２：爆弾を移動させる
-        yoko, tate = check_bound(bd_rct)
-        if not yoko:  # 練習４：横方向にはみ出たら
-            vx *= -1
-        if not tate:  # 練習４：縦方向にはみ出たら
-            vy *= -1
-        screen.blit(bd_img, bd_rct)  # 練習１：Rectを使って試しにblit
+        sum_mv, rotation_angle, flip = move_character(kk_rct, delta)
+        vx, vy = move_bomb(bd_rct, vx, vy)
+        draw_images(screen, bg_img, kk_img, kk_rct, bd_img, bd_rct, rotation_angle, flip)
 
         pg.display.update()
         tmr += 1
