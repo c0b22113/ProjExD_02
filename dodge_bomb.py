92e72d3d5f575fb1cd01c8d1d5ac15a1179ffa10
c0b22113 <c0b2211320@edu.teu.ac.jp>
2023-10-03 14:46:53 +0900

    練習問題 3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -2,16 +2,30 @@ import random
 import sys
 import pygame as pg
 
+
 WIDTH, HEIGHT = 1600, 900
+
+
+delta = { # 練習3：キー入力に対する移動量
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0),
+}
 
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
+
+    """こうかとん"""
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_img_rct = kk_img.get_rect()
+    kk_img_rct.center = (900, 400) # こうかとんの初期位置
 
+    """爆弾"""
     bomb = pg.Surface((20, 20))
     bomb.set_colorkey((0, 0, 0))
     pg.draw.circle(bomb, (255, 0, 0), (10, 10), 10)
@@ -29,7 +43,19 @@ def main():
                 return
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+
+        """こうかとん"""
+        key_lst = pg.key.get_pressed()
+        total_mv = [0, 0]
+        for key, mv in delta.items():
+            if key_lst[key]:
+                total_mv[0] += mv[0]
+                total_mv[1] += mv[1]
+
+        screen.blit(kk_img, kk_img_rct)
+        kk_img_rct.move_ip(total_mv[0], total_mv[1]) # こうかとんの移動
+
+        """爆弾"""
         screen.blit(bomb, bomb_rct)
         bomb_rct.move_ip(vx, vy) # bombの移動
 
